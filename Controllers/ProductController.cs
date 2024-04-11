11cf45c2485b2c5319a4b2e2fecee14b888e519a
42null <averyparanoiduser@gmail.com>
2024-04-11 18:31:59 -0500

    Added back discounts & Working Clone

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -7,4 +7,7 @@ public class ProductController : Controller
   public ProductController(DataContext db) => _dataContext = db;
   public IActionResult Category() => View(_dataContext.Categories.OrderBy(c => c.CategoryName));
   public IActionResult Index(int id) => View(_dataContext.Products.Where(p => p.CategoryId == id && p.Discontinued == false).OrderBy(p => p.ProductName));
+
+  public IActionResult Discounts() => View(_dataContext.Discounts.Where(d => d.EndTime > DateTime.Today).OrderBy(d => d.EndTime));
+
 }
---
+++ DropAll.sql
@@ -0,0 +1,35 @@
+DROP TABLE [dbo].[EmployeeTerritories]
+GO
+
+DROP TABLE [dbo].[OrderDetails]
+GO
+
+DROP TABLE [dbo].[Orders]
+GO
+
+DROP TABLE [dbo].[Discounts]
+GO
+
+DROP TABLE [dbo].[Products]
+GO
+
+DROP TABLE [dbo].[Territories]
+GO
+
+DROP TABLE [dbo].[Region]
+GO
+
+DROP TABLE [dbo].[Shippers]
+GO
+
+DROP TABLE [dbo].[Suppliers]
+GO
+
+DROP TABLE [dbo].[Employees]
+GO
+
+DROP TABLE [dbo].[Customers]
+GO
+
+DROP TABLE [dbo].[Categories]
+GO
