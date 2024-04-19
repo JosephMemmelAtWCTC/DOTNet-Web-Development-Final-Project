21ceb3b23fadc2ae328c441f7aeb11cd680aa351
42null <averyparanoiduser@gmail.com>
2024-04-18 21:53:06 -0500

    _Layout re-add Current Discounts

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
 
 public class ProductController : Controller
 {
@@ -7,6 +10,10 @@ public class ProductController : Controller
   public ProductController(DataContext db) => _dataContext = db;
   public IActionResult Category() => View(_dataContext.Categories.OrderBy(c => c.CategoryName));
   // public IActionResult Index(int id) => View(_dataContext.Products.Where(p => p.CategoryId == id && p.Discontinued == false).OrderBy(p => p.ProductName));
+
+    public IActionResult Discounts() => View(_dataContext.Discounts.Where(d => d.EndTime > DateTime.Today).OrderBy(d => d.EndTime));
+
+
   public IActionResult Index(int id){
     ViewBag.id = id;
     return View(_dataContext.Categories.OrderBy(c => c.CategoryName));
