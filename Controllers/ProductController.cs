f3b979406b806d1a993576165a0ffe75d9ff4e3a
42null <averyparanoiduser@gmail.com>
2024-04-18 19:32:51 -0500

    On-par with end of module 12 github version

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -6,8 +6,9 @@ public class ProductController : Controller
   private DataContext _dataContext;
   public ProductController(DataContext db) => _dataContext = db;
   public IActionResult Category() => View(_dataContext.Categories.OrderBy(c => c.CategoryName));
-  public IActionResult Index(int id) => View(_dataContext.Products.Where(p => p.CategoryId == id && p.Discontinued == false).OrderBy(p => p.ProductName));
-
-  public IActionResult Discounts() => View(_dataContext.Discounts.Where(d => d.EndTime > DateTime.Today).OrderBy(d => d.EndTime));
-
+  // public IActionResult Index(int id) => View(_dataContext.Products.Where(p => p.CategoryId == id && p.Discontinued == false).OrderBy(p => p.ProductName));
+  public IActionResult Index(int id){
+    ViewBag.id = id;
+    return View(_dataContext.Categories.OrderBy(c => c.CategoryName));
+  }
 }
