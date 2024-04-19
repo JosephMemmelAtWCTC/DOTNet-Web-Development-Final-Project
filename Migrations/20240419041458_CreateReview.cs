73136d6584f6468ef53a3a22eb6c52c55c1af1d3
42null <averyparanoiduser@gmail.com>
2024-04-18 23:15:57 -0500

    Review Migrations (CreateReview)

---
+++ Migrations/20240419041458_CreateReview.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Northwind.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateReview : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Reviews",
+                columns: table => new
+                {
+                    ReviewId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ReviewAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Rating = table.Column<byte>(type: "tinyint", nullable: false),
+                    Comment = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    CustomerId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reviews", x => x.ReviewId);
+                    table.ForeignKey(
+                        name: "FK_Reviews_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "CustomerId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Reviews_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_CustomerId",
+                table: "Reviews",
+                column: "CustomerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_ProductId",
+                table: "Reviews",
+                column: "ProductId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reviews");
+        }
+    }
+}
---
+++ Models/Review.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class Review
+{
+    // Unique to Rating
+    public int ReviewId { get; set; }
+    public DateTime ReviewAt { get; set; }
+
+    [Range(0, 5, ErrorMessage = "Value for {0} must be between {1} and {2}.")] // Limit to 0-5 for stars
+    public byte Rating { get; set; } // unsigned 8-bit range is (0-255)
+
+    public String Comment { get; set; }
+
+
+    // Linking / refrenceing to other data
+    public int ProductId { get; set; }
+    public Product Product { get; set; }
+
+    public int CustomerId { get; set; }
+    public Customer Customer { get; set; }
+
+}
