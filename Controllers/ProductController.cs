7de9703843ec8e6c5d50b043464f3cd0872d4a8e
42null <averyparanoiduser@gmail.com>
2024-05-09 20:29:19 -0500

    First working save review with both customer id & product id using link from purchases

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,13 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
 using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 
 
-public class ProductController : Controller
+// public class ProductController : Controller
+public class ProductController(DataContext db, UserManager<AppUser> usrMgr) : Controller
+
 {
+  private readonly DataContext _dataContext = db;
+  private readonly UserManager<AppUser> _userManager = usrMgr;
+
   // this controller depends on the NorthwindRepository
-  private DataContext _dataContext;
-  public ProductController(DataContext db) => _dataContext = db;
+  // private DataContext _dataContext;
+  // public ProductController(DataContext db) => _dataContext = db;
   public IActionResult Category() => View(_dataContext.Categories.OrderBy(c => c.CategoryName));
   // public IActionResult Index(int id) => View(_dataContext.Products.Where(p => p.CategoryId == id && p.Discontinued == false).OrderBy(p => p.ProductName));
 
@@ -19,7 +27,19 @@ public class ProductController : Controller
     return View(_dataContext.Categories.OrderBy(c => c.CategoryName));
   }
 
-  public IActionResult Review() => View();
+  public IActionResult Review(int id){
+    return View(
+      new Review
+        {
+          ReviewAt = DateTime.Now,
+          Rating = 0,
+          Comment = "",
+          ProductId = id,
+          CustomerId = _dataContext.Customers.FirstOrDefault(c => c.Email == User.Identity.Name).CustomerId,
+        }
+    );
+  }
+
   [HttpPost]
   [ValidateAntiForgeryToken]
   public async System.Threading.Tasks.Task<IActionResult> Review(Review newReview)
@@ -31,11 +51,9 @@ public class ProductController : Controller
           ReviewAt = DateTime.Now,
           Rating = newReview.Rating,
           Comment = newReview.Comment,
-          ProductId = 1,
-          CustomerId = 1,
+          ProductId = newReview.ProductId,
+          CustomerId = newReview.CustomerId,
         };
-        // TODO: Put back equilivent
-        // if (_dataContext.Customers.Any(c => c.CompanyName == customer.CompanyName))
 
         _dataContext.AddReview(review);
       }
